19eac06bb3a507c0770cd6f314eb394c857a5598
ThompsonA93 <thauer@edu.aau.at>
2022-04-28 04:34:37 +0200

    [#5] Automated Push-Action :: Do 28 Apr 2022 04:34:25 CEST
    Setup on Dev-Branch

--- test/BallotArchive.js
+++ test/BallotArchive.js
@@ -3,7 +3,6 @@ const BallotArchiveContract = artifacts.require('BallotArchive');
 contract('BallotArchive', () => {
     it('Test proper deployment onto chain', async() => {
         const ballotArchiveContract = await BallotArchiveContract.deployed();
-        console.log(ballotArchiveContract.address);
         assert(ballotArchiveContract.address !== '');
     });
 });
