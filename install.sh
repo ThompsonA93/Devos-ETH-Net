07e611e0df9d0a1b61700f9f57c1deea48df6c8d
ThompsonA93 <thauer@edu.aau.at>
2022-05-10 12:38:26 +0200

    Hotfix#5: EACCES: Permission error test-fix

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # NPM as prerequisite to HardHat
+echo "Installing Node"
 apt install nodejs npm
 
 # Hardhat
+echo "Installing HardHat-Suite"
 npm install --save-dev hardhat
+
+# Setup Project
+echo "Installing local Project"
+npm i
