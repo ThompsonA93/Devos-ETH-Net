e8945116d683905d05eb0d039facd655dde7de7a
ThompsonA93 <thauer@edu.aau.at>
2022-05-10 12:44:56 +0200

    Hotfix #X: Permission error due to attempted-Root Access denied (which makes sense)

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # NPM as prerequisite to HardHat
-echo "Installing Node"
+echo "#1 Installing Node"
 apt install nodejs npm
 
 # Hardhat
-echo "Installing HardHat-Suite"
+echo "#2 Installing HardHat-Suite"
 npm install --save-dev hardhat
 
 # Setup Project
-echo "Installing local Project"
+echo "#3 Installing local Project"
 npm i
